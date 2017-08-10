a66a107f2b56063dfe9ccf9dede1318a2dba12a9
Sarah Shuey <sarah.shuey@me.com>
2017-08-10 14:22:35 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,42 @@
+/*
+  Here is a rough idea for the steps you could take:
+*/
+
+// 1. First select and store the elements you'll be working with
+// 2. Create your `submit` event for getting the user's search term
+// 3. Create your `fetch` request that is called after a submission
+// 4. Create a way to append the fetch results to your page
+// 5. Create a way to listen for a click that will play the song in the audio play
+let button = document.querySelector('.search-button')
+      button.addEventListener('click', function () {
+        let input = document.querySelector('.search-input');
+        console.log('value = ', input.value);
+        let music = input.value
+let results = document.querySelector('.results')
+let url ='https://itunes.apple.com/search?term='
+
+fetch(`${url}${music}`)
+.then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log('data', data);
+        let allResults =''
+        console.log('all results', allResults);
+          data.results.forEach(function(a) {
+              allResults += `<div class ='eachResult'>
+              <div class ='name'>${a.artistName}</div>
+              <div class ='albumName'>${a.collectionName}</div>
+              <div class ='songName'>${a.trackName}</div>
+              <div class ='albumCover'><img src='${a.artworkUrl100}'></div>
+              </div>`
+              results.innerHTML = allResults
+            })
+          }
+        )
+      })
+      });
