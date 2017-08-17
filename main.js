586fb0bdab41aa645d791a07755b14fda18f196f
Sarah Shuey <sarah.shuey@me.com>
2017-08-17 18:31:16 -0500

    player working

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
-
-// 1. First select and store the elements you'll be working with
-// 2. Create your `submit` event for getting the user's search term
-// 3. Create your `fetch` request that is called after a submission
-// 4. Create a way to append the fetch results to your page
-// 5. Create a way to listen for a click that will play the song in the audio play
 let button = document.querySelector('.search-button')
       button.addEventListener('click', function () {
         let input = document.querySelector('.search-input');
         console.log('value = ', input.value);
         let music = input.value
+        let player = document.querySelector('.music-player')
 let results = document.querySelector('.results')
 let url ='https://itunes.apple.com/search?term='
 
@@ -28,17 +20,23 @@ fetch(`${url}${music}`)
         let allResults =''
         console.log('all results', allResults);
           data.results.forEach(function(a) {
-              allResults += `<div class ='eachResult'>
-              <div class ='albumCover'><a href="${a.previewUrl}"><img src='${a.artworkUrl100}'></a></div>
+            let bigdiv = document.createElement('div')
+            bigdiv.classList.add('eachResult')
+              allResults = `
+              <div class ='albumCover'><img src='${a.artworkUrl100}'></div>
               <div class ='info'>
               <div class ='songName'> ${a.trackName}</div>
               <div class ='albumName'> ${a.collectionName}</div>
               <div class ='name'> ${a.artistName}</div>
-              </div>
               </div>`
-              results.innerHTML = allResults
+              bigdiv.innerHTML = allResults
+              results.appendChild(bigdiv)
+              bigdiv.addEventListener('click', function() {
+                console.log('click');
+                console.log(a.previewUrl);
+                player.src = a.previewUrl
+              })
             })
-          }
-        )
+          })
       })
       });
